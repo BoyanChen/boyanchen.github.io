85141b3943a704bfc8e2ab80582dcdb554739755
Boyan Chen <rc99nts@gmail.com>
2018-09-19 13:31:12 -0400

    1D writing updated

--- ms1/js.js
+++ ms1/js.js
@@ -64,43 +64,37 @@ function changeDays(clicked_id){
             b[i].style.color = "black";
         }
     }
-    // document.getElementsByClassName("dayContainer").style.display = "none";
-    // document.getElementById(id).style.display = "block";
-    // switch (id) {
-    //     case "day1":
-    //         document.getElementById("dayContainer1").style.display = "block";
-    //         document.getElementById("showCase2").style.display = "none";
-    //         document.getElementById("showCase3").style.display = "none";
-    //         document.getElementById("showCase4").style.display = "none";
-    //         document.getElementById("showCase5").style.display = "none";
-    //         break;
-    //     case "b2":
-    //         document.getElementById("showCase2").style.display = "block";
-    //         document.getElementById("showCase1").style.display = "none";
-    //         document.getElementById("showCase3").style.display = "none";
-    //         document.getElementById("showCase4").style.display = "none";
-    //         document.getElementById("showCase5").style.display = "none";
-    //         break;
-    //     case "b3":
-    //         document.getElementById("showCase3").style.display = "block";
-    //         document.getElementById("showCase2").style.display = "none";
-    //         document.getElementById("showCase1").style.display = "none";
-    //         document.getElementById("showCase4").style.display = "none";
-    //         document.getElementById("showCase5").style.display = "none";
-    //         break;
-    //     case "b4":
-    //         document.getElementById("showCase4").style.display = "block";
-    //         document.getElementById("showCase2").style.display = "none";
-    //         document.getElementById("showCase3").style.display = "none";
-    //         document.getElementById("showCase1").style.display = "none";
-    //         document.getElementById("showCase5").style.display = "none";
-    //         break;
-    //     case "b5":
-    //         document.getElementById("showCase5").style.display = "block";
-    //         document.getElementById("showCase2").style.display = "none";
-    //         document.getElementById("showCase3").style.display = "none";
-    //         document.getElementById("showCase4").style.display = "none";
-    //         document.getElementById("showCase1").style.display = "none";
-    //         break;
-    // }
+
+}
+
+function changeConsequences(clicked_id) {
+
+    var id = clicked_id + "Container";
+    var btnId = clicked_id;
+
+    var id = clicked_id+"Container";
+    var btnId = clicked_id;
+
+    var a = document.getElementsByClassName("concequencesContainer");
+    var b = document.getElementsByClassName("consequences");
+    console.log(b);
+    for(var i = 0; i < a.length;i++) {
+        if (a[i].id == id) {
+            a[i].style.display = "block";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+
+    for(var i = 0; i < b.length; i++){
+        console.log(b[i]);
+        if(b[i].id == btnId){
+            b[i].style.textDecoration = "line";
+            b[i].style.color = "red";
+        }else{
+            b[i].style.textDecoration = "none";
+            b[i].style.color = "black";
+        }
+    }
+
 }
